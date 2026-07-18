7c70aeb2063b44b18c91b8a01c8b556d637dad19
2020y <2020yvwvy2020@gmail.com>
2026-07-18 09:03:52 +0300

    Enable Yandex Metrika webvisor for counter tracking.

--- src/components/YandexMetrika.tsx
+++ src/components/YandexMetrika.tsx
@@ -14,7 +14,12 @@ m[i].l=1*new Date();
 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-ym(${JSON.stringify(id)}, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
+ym(${JSON.stringify(id)}, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true
+});
 `}</Script>
       <noscript>
         <div>
