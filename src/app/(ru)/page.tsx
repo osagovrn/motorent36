cc25a32cae1f0b1edcc7f4e0411eab89cce5e475
Оsagovrn <2020yvwvy2020@gmail.com>
2026-08-15 03:18:44 +0300

    Delete src directory

--- src/app/(ru)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { HomeContent } from "@/content/HomeContent";
-import { getDictionary } from "@/i18n/dictionaries";
-
-export default function HomePage() {
-  return <HomeContent locale="ru" dict={getDictionary("ru")} />;
-}
--- src/components/SiteChrome.tsx
+++
@@ -1,3 +0,0 @@
-/** Barrel: header (client) + footer (server). */
-export { SiteHeader } from "@/components/SiteHeader";
-export { SiteFooter } from "@/components/SiteFooter";
